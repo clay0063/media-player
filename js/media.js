e4f07dc68194bd9e991661f9b4b8c02172e69593
Teagan Clayton <clay0063@algonquinlive.com>
2023-01-22 18:59:29 -0500

    First commit

---
+++ js/media.js
@@ -0,0 +1,60 @@
+const MEDIA = [
+    {
+        artist: 'MASTER BOOT RECORD',
+        title: '80386',
+        track: '80386.mp3',
+        thumbnail: 'personal-computer-thumb.jpeg',
+        large: 'personal-computer.jpeg',
+    },
+    {
+        artist: 'Baiuca & Alba Reche',
+        title: 'Diamante',
+        track: 'diamante.mp3',
+        thumbnail: 'diamante-thumb.jpeg',
+        large: 'diamante.jpeg',
+    },
+    {
+        artist: 'Sobs',
+        title: 'LOML',
+        track: 'LOML.mp3',
+        thumbnail: 'air-guitar-thumb.jpeg',
+        large: 'air-guitar.jpeg',
+    },
+    {
+        artist: 'Milk Tank',
+        title: 'Plastic New York',
+        track: 'plastic-new-york.mp3',
+        thumbnail: 'milk-tank-thumb.jpeg',
+        large: 'milk-tank.jpeg',
+    },
+    {
+        artist: 'MIRINN',
+        title: 'ステラグロウ',
+        track: 'stella-glow.mp3',
+        thumbnail: 'mirinn-thumb.jpeg',
+        large: 'mirinn.jpeg',
+    },
+    {
+        artist: 'Jack Goldstein',
+        title: 'SAILIN',
+        track: 'sailin.mp3',
+        thumbnail: 'world-is-ending-thumb.jpeg',
+        large: 'world-is-ending.jpeg',
+    },
+    {
+        artist: 'Airy',
+        title: 'In My Flickers',
+        track: 'in-my-flickers.mp3',
+        thumbnail: 'we-do-it-together-thumb.jpeg',
+        large: 'we-do-it-together.jpeg',
+    },
+    {
+        artist: 'upusen',
+        title: 'Signal',
+        track: 'signal.mp3',
+        thumbnail: 'signal-thumb.jpeg',
+        large: 'signal.jpeg',
+    },
+];
+
+export default MEDIA;
